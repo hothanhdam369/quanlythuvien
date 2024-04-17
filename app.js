1840fa755281806ed13b79872ea5e74bbb54f5fb
hothanhdam369 <damb2005668@student.ctu.edu.vn>
2024-04-17 21:25:13 +0700

    Xu ly 404

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const bookssRouter = require("./app/routes/book.route");
-// const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.json({message:"Welcome to QUAN LY THU VIEN application"});
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to QUAN LY THU VIEN application" });
 });
 app.use("/api/books", bookssRouter)
- // handle 404 response
-// app.use((req, res, next) => {
-//     return next(new ApiError(404, "Resource not found"));
-// });
-// app.use((err, req, res, next) => {
-//    return res.status(err.statusCode || 500).json({
-//     message: err.message});
-//    });
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message
+    });
+});
 
-module.exports=app;
+module.exports = app;
